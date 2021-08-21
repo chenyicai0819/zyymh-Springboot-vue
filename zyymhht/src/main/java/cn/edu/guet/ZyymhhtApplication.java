e4e901c4da7015c783ab3494eb6eba3a9d2d9203
chenyicai0819 <2495842975@qq.com>
2021-08-21 20:03:03 +0800

    添加：加入了api结构，Springboot+mybatis

---
+++ zyymhht/src/main/java/cn/edu/guet/ZyymhhtApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.guet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZyymhhtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZyymhhtApplication.class, args);
+	}
+
+}
---
+++ zyymhht/src/test/java/cn/edu/guet/ZyymhhtApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.guet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZyymhhtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
